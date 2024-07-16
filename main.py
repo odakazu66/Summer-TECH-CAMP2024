4524f5cc8c6e86754d5244f6c600b7fb53489324
tamakikouzi <kumagai.yuya.ud@tut.jp>
2024-07-16 16:55:48 +0900

    feat : Preparations for conversation loop

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ import argparse
 from modules.transcribe import transcribe_file
 from modules.chat import get_completion
 from modules.synthesize import synthesize_speech
+from modules.record import record_audio
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="音声ファイルを文字起こしして翻訳し、音声に合成するスクリプト")
-    parser.add_argument("path", help="文字起こしする音声ファイルのファイルパスまたは GCS パス")
-    args = parser.parse_args()
-
-    transcript = transcribe_file(args.path)
+    wav_path = record_audio()
+    transcript = transcribe_file(wav_path)
     completion = get_completion(transcript)
     synthesize_speech(completion, "output.wav")
