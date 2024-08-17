d4aadae5d7725b13500b1cf68426586c92e5f354
oukoutdam <outdam.ouk103@gmail.com>
2024-08-17 23:25:08 +0900

    change: Simplify Icon Selection in User Dialog

--- src/gui/user_profile_dialog.py
+++ src/gui/user_profile_dialog.py
@@ -1,3 +1,5 @@
+from PyQt5.QtCore import Qt, QSize
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QDialog, QDialogButtonBox, QVBoxLayout, QLabel, QLineEdit, QPushButton, QFileDialog
 
 
@@ -7,17 +9,20 @@ class UserProfileDialog(QDialog):
         self.parent = parent
         self.id = "You"
         self.old_user_name = parent.user_name
+        self.new_icon_path = ""
 
         layout = QVBoxLayout()
         self.setWindowTitle("User Settings")
 
+        self.icon_selection = QPushButton()
+        self.set_icon_in_dialog(self.parent.user_icon_path)
+        self.icon_selection.setStyleSheet("border: none;")
+        self.icon_selection.setCursor(Qt.PointingHandCursor)
+        self.icon_selection.clicked.connect(self.select_icon_dialog)
+
         name_label = QLabel("Name:")
         self.name_label_entry = QLineEdit()
         self.name_label_entry.setPlaceholderText(self.old_user_name)
-
-        self.path_input = QLineEdit(self)
-        self.browse_button = QPushButton("Browse...", self)
-        self.browse_button.clicked.connect(self.browse_file)
 
         q_buttons = QDialogButtonBox.Apply | QDialogButtonBox.Discard
         self.button_box = QDialogButtonBox(q_buttons)
@@ -26,14 +31,17 @@ class UserProfileDialog(QDialog):
         self.button_box.button(QDialogButtonBox.Discard).clicked.connect(self.discard)
 
         # Set up the layout
+        layout.addWidget(self.icon_selection)
         layout.addWidget(name_label)
         layout.addWidget(self.name_label_entry)
-        layout.addWidget(self.path_input)
-        layout.addWidget(self.browse_button)
         layout.addWidget(self.button_box)
         self.setLayout(layout)
 
-    def browse_file(self):
+    def set_icon_in_dialog(self, icon_path):
+        self.icon_selection.setIcon(QIcon(icon_path))
+        self.icon_selection.setIconSize(QSize(50, 50))
+
+    def select_icon_dialog(self):
         options = QFileDialog.Options()
         image_filter = "Image Files (*.png *.jpg *.jpeg *.bmp);;All Files (*)"
         file_path, _ = QFileDialog.getOpenFileName(
@@ -44,8 +52,9 @@ class UserProfileDialog(QDialog):
             options=options
         )
         if file_path:
-            self.path_input.setText(file_path)
-        print("new image path", self.path_input.text())
+            self.new_icon_path = file_path
+        print("new image path", self.new_icon_path)
+        self.set_icon_in_dialog(self.new_icon_path)
 
     def apply(self):
         print("Apply button clicked")
@@ -53,8 +62,8 @@ class UserProfileDialog(QDialog):
         if self.name_label_entry.text().strip() != "":
             self.parent.update_chat_names(self.id, self.old_user_name, self.name_label_entry.text())
 
-        if self.path_input.text() != "":
-            self.parent.update_user_icons(self.id, self.path_input.text())
+        if self.new_icon_path != "":
+            self.parent.update_user_icons(self.id, self.new_icon_path)
 
         self.accept()  # Close the dialog with an accept result
 
